d1a838f6ee657f36a6272e2beb58378655cb3074
wangbo <activesys.wb@gmail.com>
2013-02-04 13:09:19 +0800

    Implement Options as Singleton.

--- src/alpaca/console/options.py
+++ src/alpaca/console/options.py
@@ -5,23 +5,46 @@ options.py
 import getopt
 import sys
 
+class OptionsInvalidError(Exception):
+    pass
+
 class Options:
-    def __init__(self, args=[]):
-        try:
-            optlist, args = getopt.getopt(args, 'o:')
-        except getopt.GetoptErr as err:
-            print(err)
-            sys.exit(2)
+    class __Instance:
+        def __init__(self):
+            self.__input_file = '-'
+            self.__output_file = '-'
 
-        for option, value in optlist:
-            if option == '-o':
-                self.output_path = value
-                break
-        else:
-            self.output_path = '-'
+        def register(self, args=[]):
+            try:
+                optlist, args = getopt.getopt(args, 'o:')
+            except getopt.GetoptError:
+                raise OptionsInvalidError()
 
-        if len(args) > 0:
-            self.input_path = args[0]
-        else:
-            self.input_path = '-'
+            for option, value in optlist:
+                if option == '-o':
+                    self.__output_file = value
+                    break
+            else:
+                self.__output_file = '-'
+
+            if len(args) > 0:
+                self.__input_file = args[0]
+            else:
+                self.__input_file = '-'
+
+        def input_file(self):
+            return self.__input_file
+        def output_file(self):
+            return self.__output_file
+
+    __instance = None
+
+    def __init__(self):
+        if not Options.__instance:
+            Options.__instance = Options.__Instance()
+
+    def __getattr__(self, attr):
+        return getattr(Options.__instance, attr)
+    def __setattr__(self, attr, value):
+        return setattr(Options.__instance, attr, value)
 
--- src/test_alpaca/test_console/test_options.py
+++ src/test_alpaca/test_console/test_options.py
@@ -6,21 +6,48 @@ import unittest
 from console import options
 
 class TestOptions(unittest.TestCase):
+    def test_register_error(self):
+        args = '-a -b -c d'.split()
+        self.assertRaises(options.OptionsInvalidError, options.Options().register, args)
+
     def test_input_file(self):
         args = 'input.regex'.split()
-        input_option = options.Options(args)
-        self.assertEqual(input_option.input_path, 'input.regex')
+        options.Options().register(args)
+        self.assertEqual(options.Options().input_file(), 'input.regex')
 
     def test_input_console(self):
-        input_option = options.Options()
-        self.assertEqual(input_option.input_path, '-')
+        options.Options().register()
+        self.assertEqual(options.Options().input_file(), '-')
 
     def test_output_file(self):
         args = '-o output.dot'.split()
-        output_option = options.Options(args)
-        self.assertEqual(output_option.output_path, 'output.dot')
+        options.Options().register(args)
+        self.assertEqual(options.Options().output_file(), 'output.dot')
 
     def test_output_console(self):
-        output_option = options.Options()
-        self.assertEqual(output_option.output_path, '-')
+        options.Options().register()
+        self.assertEqual(options.Options().output_file(), '-')
+
+    def test_io_file(self):
+        args = '-o output.dot input.regex'.split()
+        options.Options().register(args)
+        self.assertEqual(options.Options().input_file(), 'input.regex')
+        self.assertEqual(options.Options().output_file(), 'output.dot')
+
+    def test_i_file_o_console(self):
+        args = 'input.regex'.split()
+        options.Options().register(args)
+        self.assertEqual(options.Options().input_file(), 'input.regex')
+        self.assertEqual(options.Options().output_file(), '-')
+
+    def test_i_console_o_file(self):
+        args = '-o output.dot'.split()
+        options.Options().register(args)
+        self.assertEqual(options.Options().input_file(), '-')
+        self.assertEqual(options.Options().output_file(), 'output.dot')
+
+    def test_io_console(self):
+        options.Options().register()
+        self.assertEqual(options.Options().input_file(), '-')
+        self.assertEqual(options.Options().output_file(), '-')
 
